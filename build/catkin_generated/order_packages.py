1dcbe3ec3d3264d6134fe7c830b3795bdb172581
programmatoroSeduto <francesc.ganci@gmail.com>
2021-06-18 13:24:31 +0000

    init ROS workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/test_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/myros_ws/devel;/home/armor_ws/devel;/home/course_ws/devel;/home/tmp_ws/devel;/opt/ros/kinetic'.split(';') if '/home/myros_ws/devel;/home/armor_ws/devel;/home/course_ws/devel;/home/tmp_ws/devel;/opt/ros/kinetic' != '' else []
